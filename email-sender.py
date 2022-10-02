6936ad31793fd8f458bb6e73c50c8f92a8a543c6
Alberto Tomasin <alberto.tomasin@gmail.com>
2022-10-02 14:18:29 +0200

    terminato programa
    le mail vengono inviate
    
    Took 14 minutes

--- email-sender.py
+++ email-sender.py
@@ -7,24 +7,41 @@ nel caso in cui venga lanciato un errore sul certificato ssl eseguire i seguenti
 from email.message import EmailMessage
 import ssl
 import smtplib
+import csv
 
-from_address = "inserire la main"
-password = "password per il commit"
-to_address = "inserire il ricevente"
+sender_mail = "mail"
+password = "password"
 
-subject = "inserire l'oggetto della mail"
-body = "inserire il corpo della mail"
+subject = "compilazione form yeswork - test"
+body = """
+Buongiorno, sono Alberto
 
-em = EmailMessage()
-em['From'] = from_address
-em['To'] = to_address
-em['Subject'] = subject
-em.set_content(body)
+Vi chiedo cortesemente il vostro aiuto nella compilazione del seguente questionario realizzato in collaborazione con la recente realtà di Yeswork, applicazione innovativa sviluppata da Il Lavoro in un Tocco s.r.l.
+
+Yeswork è un’app che rivoluziona la ricerca del lavoro permettendo ad imprese, lavoratori e studenti di domandare e offrire prestazioni lavorative occasionali in momenti di necessità e senza vincolo contrattuale, retribuito e nel rispetto della legge. 
+ 
+La vision di Yeswork è quella di “creare una Community di milioni di persone per migliorare la loro vita e quella degli altri, risolvendo problematiche quotidiane”. 
+Al fine di realizzare un’accurata ricerca di mercato, vi chiedo gentilmente di dedicare 5 minuti alla compilazione del questionario che allego.
+
+https://so7rs123rmm.typeform.com/to/QLy5Fixa
+
+Vi ringrazio anticipatamente e vi porgo cordiali saluti.
+
+"""
+
 
 context = ssl.create_default_context()
-
-with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
-    smtp.login(from_address, password)
-    smtp.sendmail(from_address, to_address, em.as_string())
-
+with open("test-email.csv") as file:
+    reader = csv.reader(file)
+    next(reader)
+    for email in reader:
+        with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+            smtp.login(sender_mail, password)
+            em = EmailMessage()
+            em['From'] = sender_mail
+            em['Subject'] = subject
+            em.set_content(body)
+            em['To'] = email
+            smtp.sendmail(sender_mail, email, em.as_string())
+            print(f"sent mail to {email}")
 print("program finished")
